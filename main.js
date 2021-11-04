aa9ddd9bfd6bbe1e2de48dcebf87393922edb8a6
guilherme-comin <182739@upf.br>
2021-11-04 17:10:54 -0300

    coisas

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll('nav .toggle')
 
 for (var element of toggle) {
-    element.addEventListener('click', function(){
-        nav.classList.toggle('show')
-    })
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
 }
 
 // Fechar quando clicar no a
@@ -13,7 +13,20 @@ for (var element of toggle) {
 const links = document.querySelectorAll('nav ul li a')
 
 for (var link of links) {
-    link.addEventListener('click', function(){
-        nav.classList.remove('show')
-    })
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
 }
+
+// Shadow after scroll on header
+
+const header = document.querySelector('#header')
+const navHeight = nav.offsetHeight
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= navHeight) {
+    header.classList.add('scroll')
+  } else {
+    header.classList.remove('scroll')
+  }
+})
